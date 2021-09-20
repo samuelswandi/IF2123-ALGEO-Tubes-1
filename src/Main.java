8e9deb771cbfaff5e4653c981ea04efc8ccdd671
samuelswandi <16520414@std.stei.itb.ac.id>
2021-09-20 23:39:06 +0700

    Membuat Input matriks dan Menu di main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,41 @@
-package src;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
+    System.out.println("Selamat Datang di Aplikasi Pengolahan Matrix!");
+    System.out.println("Sebelum masuk dan mengolah sebuah matrix, silahkan masukkan matrix lebih dahulu");
+
+    // Input matrix
+    System.out.print("Masukkan ukuran matriks: ");
+    Scanner in = new Scanner(System.in);
+    int rows = in.nextInt();
+    int columns = in.nextInt();
+
+    System.out.println("Masukkan ukuran matriks : ");
+
+    int matrix[][] = new int[rows][columns];
+
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < columns; j++) {
+        matrix[i][j] = in.nextInt();
+      }
+    }
+    System.out.print("\nMatriks yang telah dimasukkan : \n");
+    for (int[] row : matrix) {
+      for (int col : row) {
+        System.out.print(col + " ");
+      }
+      System.out.println();
+    }
+    in.close();
+
+    System.out.println("Selamat telah memasukkan matrix!");
+    System.out.println("Sekarang mari berangkat ke menu pengolahan matriks!");
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    System.out.println();
+    
     System.out.println("MENU");
     System.out.println("1. Sistem Persamaan Linier");
     System.out.println("2. Determinan");
@@ -10,6 +44,5 @@ public class Main {
     System.out.println("5. Regresi linear berganda");
     System.out.println("6. Keluar");
 
-    
   }
 }
