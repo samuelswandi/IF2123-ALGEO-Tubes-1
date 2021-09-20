7f4603b4a87dcd9c0670e6bc3981136b20a8981a
samuelswandi <16520414@std.stei.itb.ac.id>
2021-09-20 23:27:09 +0700

    add Main and Matrix

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("MENU");
+    System.out.println("1. Sistem Persamaan Linier");
+    System.out.println("2. Determinan");
+    System.out.println("3. Matriks balikan");
+    System.out.println("4. Interpolasi Polinom");
+    System.out.println("5. Regresi linear berganda");
+    System.out.println("6. Keluar");
+
+    
+  }
+}
